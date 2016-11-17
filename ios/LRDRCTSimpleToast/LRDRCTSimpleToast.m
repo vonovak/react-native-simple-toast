02a66bc4aef66608d570f5eb4628c077c571f212
Quentin Valmori <quentin.valmori@gmail.com>
2016-11-17 14:52:54 +0100

    Update LRDRCTSimpleToast.m

--- ios/LRDRCTSimpleToast/LRDRCTSimpleToast.m
+++ ios/LRDRCTSimpleToast/LRDRCTSimpleToast.m
@@ -73,9 +73,37 @@ RCT_EXPORT_METHOD(showWithGravity:(NSString *)msg duration:(double)duration grav
     [self _show:msg duration:duration gravity:gravity.intValue];
 });
 
+- (UIViewController *)visibleViewController:(UIViewController *)rootViewController
+{
+    if (rootViewController.presentedViewController == nil)
+    {
+        return rootViewController;
+    }
+    if ([rootViewController.presentedViewController isKindOfClass:[UINavigationController class]])
+    {
+        UINavigationController *navigationController = (UINavigationController *)rootViewController.presentedViewController;
+        UIViewController *lastViewController = [[navigationController viewControllers] lastObject];
+        
+        return [self visibleViewController:lastViewController];
+    }
+    if ([rootViewController.presentedViewController isKindOfClass:[UITabBarController class]])
+    {
+        UITabBarController *tabBarController = (UITabBarController *)rootViewController.presentedViewController;
+        UIViewController *selectedViewController = tabBarController.selectedViewController;
+        
+        return [self visibleViewController:selectedViewController];
+    }
+    
+    UIViewController *presentedViewController = (UIViewController *)rootViewController.presentedViewController;
+    
+    return [self visibleViewController:presentedViewController];
+}
+
 - (void)_show:(NSString *)msg duration:(NSTimeInterval)duration gravity:(NSInteger)gravity {
     dispatch_async(dispatch_get_main_queue(), ^{
-        UIView *root = [[[[[UIApplication sharedApplication] delegate] window] rootViewController] view];
+        //UIView *root = [[[[[UIApplication sharedApplication] delegate] window] rootViewController] view];
+        UIViewController *ctrl = [self visibleViewController:[UIApplication sharedApplication].keyWindow.rootViewController];
+        UIView *root = [ctrl view];
         CGRect bound = root.bounds;
         bound.size.height -= _keyOffset;
         if (bound.size.height > LRDRCTSimpleToastBottomOffset*2) {
