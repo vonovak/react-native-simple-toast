69770a8b50b4c5e46d814d841f935a54b3de2a56
Maksim Efimov <mg.efimov97@gmail.com>
2024-02-22 15:02:33 +0300

    refactor: toast window

--- ios/RNToastWindow.m
+++ ios/RNToastWindow.m
@@ -1,6 +1,32 @@
 #import "RNToastWindow.h"
+#import <React/RCTUtils.h>
 
 @implementation RNToastWindow
+
+- (instancetype)init
+{
+    if (@available(iOS 13.0, *)) {
+        for (UIScene *scene in RCTSharedApplication().connectedScenes) {
+            if (scene.activationState == UISceneActivationStateForegroundActive &&
+                [scene isKindOfClass:[UIWindowScene class]]) {
+                self = [super initWithWindowScene:(UIWindowScene *)scene];
+            }
+        }
+    }
+    if (!self) {
+        UIWindow *keyWindow = RCTSharedApplication().keyWindow;
+        if (keyWindow) {
+            self = [super initWithFrame:keyWindow.bounds];
+        } else {
+            // keyWindow is nil, so we cannot create and initialize _toastWindow
+            NSLog(@"Unable to create alert window: keyWindow is nil");
+        }
+    }
+    if (self) {
+        [self setHidden:NO];
+    }
+    return self;
+}
 
 - (instancetype)initWithFrame:(CGRect)frame {
     if (self = [super initWithFrame:frame]) {
@@ -20,6 +46,13 @@
   id hitView = [super hitTest:point withEvent:event];
   if (hitView == self) return nil;
   else return hitView;
+}
+
+- (void)dealloc
+{
+    if (@available(iOS 13, *)) {
+        self.windowScene = nil;
+    }
 }
 
 @end
