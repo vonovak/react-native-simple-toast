427aeafad0b098e78b56b35e3381177db4bdeb7f
Vojtech Novak <vonovak@gmail.com>
2024-03-20 09:17:45 +0100

    Revert "feat: remove unneeded view controller (#74)"
    This reverts commit fb78459b68d08e1e9b144724011bf2021349033d.

--- ios/RNToastWindow.m
+++ ios/RNToastWindow.m
@@ -1,29 +1,16 @@
 #import "RNToastWindow.h"
-#import <React/RCTUtils.h>
 
 @implementation RNToastWindow
 
-- (instancetype)init
-{
-    if (@available(iOS 13.0, *)) {
-        for (UIScene *scene in RCTSharedApplication().connectedScenes) {
-            if (scene.activationState == UISceneActivationStateForegroundActive &&
-                [scene isKindOfClass:[UIWindowScene class]]) {
-                self = [super initWithWindowScene:(UIWindowScene *)scene];
-            }
-        }
+- (instancetype)initWithFrame:(CGRect)frame {
+    if (self = [super initWithFrame:frame]) {
+        self.windowLevel = UIWindowLevelAlert + 1;
     }
-    if (!self) {
-        UIWindow *keyWindow = RCTSharedApplication().keyWindow;
-        if (keyWindow) {
-            self = [super initWithFrame:keyWindow.bounds];
-        } else {
-            // keyWindow is nil, so we cannot create and initialize _toastWindow
-            NSLog(@"Unable to create alert window: keyWindow is nil");
-        }
-    }
-    if (self) {
-        [self setHidden:NO];
+    return self;
+}
+
+- (instancetype)initWithWindowScene:(UIWindowScene *)windowScene {
+    if (self = [super initWithWindowScene:windowScene]) {
         self.windowLevel = UIWindowLevelAlert + 1;
     }
     return self;
@@ -33,13 +20,6 @@
   id hitView = [super hitTest:point withEvent:event];
   if (hitView == self) return nil;
   else return hitView;
-}
-
-- (void)dealloc
-{
-    if (@available(iOS 13, *)) {
-        self.windowScene = nil;
-    }
 }
 
 @end
